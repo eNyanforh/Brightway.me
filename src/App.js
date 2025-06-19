902c2d9105568350e7e04bdd786de5356bc0ce68
eNyanforh <nyanforhen@gmail.com>
2025-06-19 09:37:18 -0700

    udated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Analytics } from "@vercel/analytics/react"
+
 
 const Container = styled.div`
   font-family: 'Inter', sans-serif;
